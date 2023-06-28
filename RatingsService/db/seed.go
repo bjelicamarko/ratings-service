c3cd592c2dc358d81c23ea1ae801fbfcba3ae52c
darkotica <58399701+darkotica@users.noreply.github.com>
2023-06-28 21:02:04 +0200

    feat: add to seed

--- RatingsService/db/seed.go
+++ RatingsService/db/seed.go
@@ -25,6 +25,24 @@ var Ratings = []models.AccommodationRating{
 		AccommodationId: 1,
 		Status:          models.ACCEPTED,
 	},
+	{
+		Model:           gorm.Model{},
+		Mark:            2,
+		Comment:         "ni tu ni tamo iskr",
+		GuestId:         4,
+		HostId:          1,
+		AccommodationId: 1,
+		Status:          models.ACCEPTED,
+	},
+	{
+		Model:           gorm.Model{},
+		Mark:            3,
+		Comment:         "nije se pretrgao",
+		GuestId:         4,
+		HostId:          2,
+		AccommodationId: 6,
+		Status:          models.ACCEPTED,
+	},
 }
 
 var HostRatings = []models.HostRating{
@@ -42,6 +60,14 @@ var HostRatings = []models.HostRating{
 		Comment: "odlicno",
 		GuestId: 5,
 		HostId:  1,
+		Status:  models.ACCEPTED,
+	},
+	{
+		Model:   gorm.Model{},
+		Mark:    4,
+		Comment: "bas je dobar bata",
+		GuestId: 2,
+		HostId:  2,
 		Status:  models.ACCEPTED,
 	},
 }
